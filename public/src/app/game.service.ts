59c62bec12b227b501a1da66acf6b24d153de013
VitalieBraga <vitaliebraga@gmail.com>
2017-09-26 17:37:57 -0700

    displaying the dashboard with all the games implemented

--- public/src/app/game.service.ts
+++ public/src/app/game.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
+import { dynamicSort } from './sortby';
 
 @Injectable()
 export class GameService {
@@ -17,18 +18,20 @@ export class GameService {
       .subscribe(data => {
         console.log("Game created", data.json());
         this.games = data.json();
+        this.games.sort(dynamicSort("-correct"));
         this.gameObserver.next(this.games);
         this._router.navigate(['dashboard']);
       }, err => {
         console.log("Create game error");
       })
-  }
-
-  allGames() {
-    this._http.get(`/game.json`)
+    }
+    
+    allGames() {
+      this._http.get(`/game.json`)
       .subscribe(data => {
         console.log("got all games", data.json());
         this.games = data.json();
+        this.games.sort(dynamicSort("-correct"));
         this.gameObserver.next(this.games);
       }, err => {
         console.log("Getting all games error: ", err)
